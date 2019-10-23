360658cc9188d40cfe3acb94b178736dd29b90fc
SafarRadim <safar.radim@gmail.com>
2019-10-23 14:52:42 +0200

    added sockets into the app reworked app.py

--- forms.py
+++ forms.py
@@ -3,10 +3,6 @@ import wtforms as wtf
 from wtforms.validators import DataRequired
 
 
-class MessageForm(FlaskForm):
-    message = wtf.StringField('Message', validators=[DataRequired()])
-
-
 class LoginForm(FlaskForm):
     username = wtf.StringField('Username', validators=[DataRequired()])
     password = wtf.PasswordField('Password', validators=[DataRequired()])
